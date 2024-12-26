ec30d8ba61d6c00e4261f49c2d3c8406fe4f7200
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-12-25 22:31:38 -0500

    add utility functions and UI components for improved styling and layout from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <div className="flex justify-center items-center flex-col gap-12">
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="destructive" size="lg" asChild>
+        <Link href="/orders">Orders</Link>
+      </Button>
+    </div>
+  );
 }
 export default HomePage;

aeddbab5ce5b9d68434caf694c3e1ecf62713f76
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2025-01-28 14:50:37 -0500

    update layout background color for improved aesthetics

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f5f5dc]`}
       >
         <Navbar />
         <Container className="py-20">{children}</Container>

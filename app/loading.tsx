ef6d94c72f47b10374c107eb72d49fdc66610f8b
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2025-03-05 18:12:46 -0500

    style: add spinning animation to loading icon

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 const loading = () => {
   return (
     <div className="flex justify-center items-center text-primary h-[calc(100vh-113.6px)]">
-      <AiOutlineLoading3Quarters className="w-16 h-16" />
+      <AiOutlineLoading3Quarters className="w-16 h-16 animate-spin" />
     </div>
   );
 };
